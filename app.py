4555fe1ae9c01278e4e3560d40622646f4e9bcfc
송기훈 <121469546+Kihoon9498@users.noreply.github.com>
2023-06-21 18:00:20 +0900

    06.21

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ import os
 import torch
 from inference import Callcenter
 
-
-
 # DB연동
 db_conn = pymysql.connect(
     host= 'localhost',
@@ -28,25 +26,26 @@ db_conn = pymysql.connect(
 print(db_conn)
 counts = dict()
 
-
-
+# 필요 함수 및 변수 정의
 category_dict = {'일반행정' : 0,
                 '코로나' : 1,
                 '생활하수도' : 2,
                 '대중교통' : 3}
+
+person_dict = {'0' : '지창욱',
+                '1' : '유재석'}
 category = -1
+person_id = 0
 
 df0 = pd.read_pickle('dbPickle/normal_emb_id.pickle')
 df1 = pd.read_pickle('dbPickle/corona_emb_id.pickle')
 df2 = pd.read_pickle('dbPickle/water_emb_id.pickle')
 df3 = pd.DataFrame()
 dfs = [df0, df1, df2, df3]
-# print('embedding loaded!')
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(device)
+print("연결 Device :", device)
 
-# emb_dir = 'datasets'
 poly_dir = 'models/poly_encoder'
 cross_dir = 'models/cross_encoder'
 cross_encoder, _ = Load_Model_Tokenizer(cross_dir, model_type='cross')
@@ -69,118 +68,6 @@ pipe = TextClassificationPipeline(
         top_k = None,
         function_to_apply = 'sigmoid')
 
-# Flask 객체 인스턴스 생성
-app = Flask(__name__)
-
-app.config["UPLOAD_DIR"] = "uploads"
-app.config["UPLOAD_FOLDER"] = "audio"
-
-recording = False
-temp_file = "temp.wav"  # 임시 파일 경로
-
-sub_stat = True
-
-@app.route('/') # 접속하는 url
-def main():
-    return render_template('first.html')
-
-@app.route('/main') # 접속하는 url
-def to_main():
-    global person_id
-    global sub_stat
-    person_id = request.args.get('person_id')
-    return render_template('main.html', sub_state = sub_stat)
-
-@app.route('/video_main') # 접속하는 url
-def video_main():
-    global category
-    category = request.args.get('category')
-
-    if 'id' in request.args:
-        answer_id = request.args.get('id')
-
-        count = counts[int(answer_id)] + 1
-        cursor = db_conn.cursor()
-
-        query = f'UPDATE faq SET count = {count} WHERE id = {answer_id}'
-
-        cursor.execute(query)
-
-        db_conn.commit()
-        
-        cursor = db_conn.cursor()
-
-        query = f'select * from path_table where id = {answer_id}'
-
-        cursor.execute(query)
-        answer_id = [{'audio' : c[1], 'video' : c[2], 'answer' : c[3]} for c in cursor][0]
-
-    else:
-        answer_id = {'audio' : 'intro.wav', 'video' : 'intro.mp4', 'answer' : '정부에서 별도 지원금을 지급하고 있습니다.'}
-
-
-    cursor = db_conn.cursor()
-    query = f'select * from faq where category = {category_dict[category]} order by count desc limit 20'
-
-    cursor.execute(query)
-    
-    result = []
-    for idx, i in enumerate(cursor):
-        counts[i[0]] = i[2]
-        result.append({'id' : i[0], 'question' : i[1], 'count' : idx})
-
-    return render_template('video_main.html', title = category, answer_id= answer_id, result= result)
-
-
-@app.route('/save-record', methods=['POST'])
-def save_record():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    file_name = 'Temp' + ".wav"
-    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
-    file.save(full_file_name)
-    # print(category)
-    emb_df = dfs[category_dict[category]]
-    # print(emb_df)
-
-    text = infer_STT(full_file_name)
-    if toxic_check(text):
-        ment = "욕설이 감지되었습니다."
-        # print(ment)
-        return ment
-    
-    print(text)
-    # for result in pipe(text)[0]:
-        # print(result)
-    top_k_cross_scores, top_k_indices = infer_POLY(text, emb_df)
-    max_idx = np.argmax(top_k_cross_scores)
-
-    if top_k_cross_scores[max_idx] > 10:
-        emb_idx = top_k_indices[max_idx]
-        answer_id = emb_df.iloc[emb_idx]['index']
-        print(answer_id)
-
-        cursor = db_conn.cursor()
-
-        query = f'select * from path_table where id = {answer_id}'
-
-        cursor.execute(query)
-        answer_id = [{'audio' : c[1], 'video' : c[2], 'answer' : c[3], 'text' : text} for c in cursor][0]
-        print(answer_id['answer'])
-        return answer_id
-
-    else:
-        answer = '시민님, 적절한 답변이 없습니다.'
-        return {'answer' : answer, 'text' : text}
-    
 def infer_STT(filename):
     global model_STT
     result = model_STT.transcribe(filename, fp16=True)
@@ -198,102 +85,149 @@ def infer_POLY(text, emb_df):
 
 def toxic_check(txt):
     result = pipe(txt)
-
     if result[0][0]['label'] != "None":
         return True
-    
     else: 
         return False
+
+# Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+app.config["UPLOAD_DIR"] = "uploads"
+app.config["UPLOAD_FOLDER"] = "audio"
+
+recording = False
+temp_file = "temp.wav"  # 임시 파일 경로
+
+sub_stat = True
+
+@app.route('/') # 상담원을 선택할 수 있는 첫 페이지
+def main():
+    return render_template('first.html')
+
+@app.route('/main') # 카테고리 선택할 수 있는 페이지
+def to_main():
+    global person_id
+    person_id = request.args.get('person_id')
+    person = person_dict[person_id]
+    return render_template('main.html', person_id=person_id, person=person)
+
+@app.route('/video_main') # 상담을 진행하는 페이지
+def video_main():
+    global person_id
+    global category
+    category = request.args.get('category')
+
+    if 'id' in request.args:
+        answer_id = request.args.get('id')
+        count = counts[int(answer_id)] + 1
+        cursor = db_conn.cursor()
+        query = f'UPDATE faq SET count = {count} WHERE id = {answer_id}'
+        cursor.execute(query)
+        db_conn.commit()
+        cursor = db_conn.cursor()
+        query = f'select * from path_table where id = {answer_id}'
+        cursor.execute(query)
+        answer_id = [{'audio' : person_id + '_' + c[1], 'video' : person_id + '_' + c[2], 'answer' : c[3]} for c in cursor][0]
+    else:
+        answer_id = {'audio' : f'{person_id}_intro2.wav', 'video' : f'{person_id}_intro2.mp4', 'answer' : '하단 중앙의 마이크 버튼을 눌러 시작, 종료하셔서 상담을 진행하실 수 있습니다.'} # 인트로용 멘트로 변경
+
+    # DB에서 필요 데이터 불러오기
+    cursor = db_conn.cursor()
+    query = f'select * from faq where category = {str(category_dict[category])} order by count desc limit 20'
+    cursor.execute(query)
     
-# @app.route('/save-text', methods=['POST'])
-# def savetext():
-#     emb_df = dfs[category_dict[category]]
+    result = []
+    for idx, i in enumerate(cursor):
+        counts[i[0]] = i[2]
+        result.append({'id' : i[0], 'question' : i[1], 'count' : idx})
 
-#     text = request.form['question']
-#     if toxic_check(text):
-#         ment = "욕설이 감지되었습니다."
-#         # print(ment)
-#         return ment
+    return render_template('video_main.html', title = category, answer_id= answer_id, result= result)
+
+
+@app.route('/video_main', methods=['POST']) # 텍스트로 질문하기
+def txt_input():
+    global person_id
+    global category
+    category = request.args.get('category')
+    print(category)
     
-#     print(text)
-#     # for result in pipe(text)[0]:
-#         # print(result)
-#     top_k_cross_scores, top_k_indices = infer_POLY(text, emb_df)
-#     max_idx = np.argmax(top_k_cross_scores)
-
-#     if top_k_cross_scores[max_idx] > 10:
-#         emb_idx = top_k_indices[max_idx]
-#         answer_id = emb_df.iloc[emb_idx]['index']
-#         print(answer_id)
-
-#         cursor = db_conn.cursor()
-
-#         query = f'select * from path_table where id = {answer_id}'
-
-#         cursor.execute(query)
-#         answer_id = [{'audio' : c[1], 'video' : c[2], 'answer' : c[3], 'text' : text} for c in cursor][0]
-#         print(answer_id['answer'])
-#         return answer_id
-
-#     else:
-#         answer = '시민님, 적절한 답변이 없습니다.'
-#         return {'answer' : answer, 'text' : text}
+    text = request.form['question']
     
-# def infer_POLY(text, emb_df):
-#     global tokenizer_poly
-#     call_center = Callcenter(poly_encoder=poly_encoder, cross_encoder=cross_encoder,
-#                         tokenizer=tokenizer_poly, emb_df=emb_df, device=device, topk= 10)
-#     top_k_cross_scores, top_k_indices = call_center.inference(text)
-#     print(top_k_cross_scores)
-#     print(top_k_indices)
-#     return top_k_cross_scores, top_k_indices
-
-# def toxic_check(txt):
-#     result = pipe(txt)
-
-#     if result[0][0]['label'] != "None":
-#         return True
+    if toxic_check(text): # 욕설 감지
+        answer = "욕설이 감지되었습니다."
+        return {'answer' : answer, 'text' : text}
     
-#     else: 
-#         return False
+    print(text)
 
-# def txt_input():
-#     global category
-#     print(f'category : {category}')
-#     text = request.form['question']
-#     if toxic_check(text):
-#         ment = "욕설이 감지되었습니다."
-#         # print(ment)
-#         return ment
+    emb_df = dfs[category_dict[category]]
+    top_k_cross_scores, top_k_indices = infer_POLY(text, emb_df)
+    max_idx = np.argmax(top_k_cross_scores)
+
+    if top_k_cross_scores[max_idx] > 10:
+        emb_idx = top_k_indices[max_idx]
+        answer_id = emb_df.iloc[emb_idx]['index']
+        print(answer_id)
+
+        cursor = db_conn.cursor()
+
+        query = f'select * from path_table where id = {answer_id}'
+
+        cursor.execute(query)
+        answer_id = [{'audio' : person_id + '_' + c[1], 'video' : person_id + '_' + c[2], 'answer' : c[3], 'text' : text} for c in cursor][0]
+        print(answer_id['answer'])
+        return answer_id
+
+    else:
+        answer = '시민님, 적절한 답변이 없습니다.'
+        return {'answer' : answer, 'text' : text}
+
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part // 녹음 존재 여부 확인
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file_name = 'Temp' + ".wav"
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    emb_df = dfs[category_dict[category]]
+    text = infer_STT(full_file_name)
     
-#     print(text)
-#     # for result in pipe(text)[0]:
-#         # print(result)
-#     emb_df = dfs[category_dict[category]]
-#     answer_id = infer_POLY(text, emb_df)
+    if toxic_check(text): # 욕설 감지
+        ment = "욕설이 감지되었습니다."
+        return ment
     
-#     print(answer_id)
+    print(text)
 
-#     cursor = db_conn.cursor()
+    top_k_cross_scores, top_k_indices = infer_POLY(text, emb_df)
+    max_idx = np.argmax(top_k_cross_scores)
 
-#     query = f'select * from path_table where id = {answer_id}'
+    if top_k_cross_scores[max_idx] > 10:
+        emb_idx = top_k_indices[max_idx]
+        answer_id = emb_df.iloc[emb_idx]['index']
+        print(answer_id)
 
-#     cursor.execute(query)
-#     answer_id = [{'audio' : c[1], 'video' : c[2], 'answer' : c[3], 'text' : text} for c in cursor][0]
+        cursor = db_conn.cursor()
+
+        query = f'select * from path_table where id = {answer_id}'
+
+        cursor.execute(query)
+        answer_id = [{'audio' : person_id + '_' + c[1], 'video' : person_id + '_' + c[2], 'answer' : c[3], 'text' : text} for c in cursor][0]
+        print(answer_id['answer'])
+        return answer_id
+
+    else:
+        answer = '시민님, 적절한 답변이 없습니다.'
+        return {'answer' : answer, 'text' : text}
     
-#     cursor = db_conn.cursor()
-#     query = f'select * from faq where category = {category_dict[category]} order by count desc limit 20'
-
-#     cursor.execute(query)
-    
-#     result = []
-#     for idx, i in enumerate(cursor):
-#         counts[i[0]] = i[2]
-#         result.append({'id' : i[0], 'question' : i[1], 'count' : idx})
-
-#     return render_template('video_main.html', title = category, answer_id= answer_id, result= result)
-
 if __name__ == '__main__':
     # app.run(debug=True)
     # host 등을 직접 지정하고 싶다면
-    app.run(host='0.0.0.0', port='8080', debug=True, threaded=True)
+    app.run(host='0.0.0.0', port='8080', debug=True, threaded=True, use_reloader=False)
